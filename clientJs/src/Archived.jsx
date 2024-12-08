0cc35fa6a2690db82a10a91f7e7b30ed7fec4710
rainn26 <xinzhao2627@gmail.com>
2024-12-08 12:37:26 +0800

    initial push

---
+++ clientJs/src/Archived.jsx
@@ -0,0 +1,93 @@
+/* eslint-disable no-unused-vars */
+
+import { Grid2, Stack } from '@mui/material';
+import archived_data from './assets/archived_data.json'
+import ITable from './components/ITable';
+import DrawerMenu from './components/DrawerMenu';
+import NavSetting from './components/NavSetting';
+function Archived() {
+    function createData(report_id, room, computer_id, components, date_submitted, submittee, building, comment, date_archived, status){
+        return {report_id, room, computer_id, components, date_submitted, submittee, building, comment, date_archived, status}
+    }
+
+    const rows = archived_data.rows.map((rd) => createData( 
+        rd.report_id, 
+        rd.room, 
+        rd.computer_id,
+        rd.components,
+        rd.date_submitted,
+        rd.submittee,
+        rd.building,
+        rd.comment,
+        rd.date_archived,
+        rd.status,
+    ))
+    const headCells = [
+        {
+            id: "report_id",
+            numeric: false,
+            disablePadding: true,
+            label: "Report ID",
+        },
+        {
+            id: "computer_id",
+            numeric: false,
+            disablePadding: true,
+            label: "Computer ID",
+        },
+        {
+            id: "status",
+            numeric: false,
+            disablePadding: true,
+            label: "Status",
+        },
+        {
+            id: "room",
+            numeric: false,
+            disablePadding: true,
+            label: "Room - Bd",
+        },
+        {
+            id: "components",
+            numeric: false,
+            disablePadding: false,
+            label: "Reported Components",
+        },
+        {
+            id: "date_submitted",
+            numeric: false,
+            disablePadding: true,
+            label: "Date Submitted",
+        },
+        {
+            id: "date_archived",
+            numeric: false,
+            disablePadding: true,
+            label: "Date Archived",
+        },
+        {
+            id: "submittee",
+            numeric: false,
+            disablePadding: true,
+            label: "Submittee",
+        },
+
+    ]
+
+
+    return <div style={{display: 'flex', height:'100vh'}}>
+    <DrawerMenu/>
+    <Stack width={'100vw'}>
+        <NavSetting/>
+        <div className='mx-4'>
+            <div className="label">
+                <div className="text-wrapper">Archived Reports</div>
+            </div>
+            <ITable headCells={headCells} rows={rows} type='archivedTable'/>
+        </div>
+    </Stack>
+
+</div>;
+}
+
+export default Archived;
---
+++ clientJs/src/assets/palette.js
@@ -0,0 +1,35 @@
+const palette = {
+    bad: '#DE454B',
+    weakHi2: '#4488BF',
+    wFbg: '#EDF0F7',
+    selected: '#4C64D9',
+    major: '#EA9317',
+    weakHi: '#4488BF',
+    wFtext: '#AOABCO',
+    textLight: '#9EA3A2',
+    fillUnlisted: '#DCD0D0',
+    minor: '#FCE5A7',
+    strongFill: '#4488BF',
+    wFhead: '#717D96',
+    textWeak: '#000933',
+    strokeWeak: '#DADADA',
+    good: '#35408E',
+    darkBlue: '#35408E',
+    wFstroke: '#2D3648',
+    txtStrong: '#000626',
+    strokeMain: '#CCCCCC',
+
+    badBg: '#F7B1B4',
+    badFont: '#7A1218',
+
+    minorBg: '#FFEDB5',
+    minorFont:'#7A5E1C',
+
+    majorBg: '#FFD099',
+    majorFont: '#8A4D0C',
+
+    goodBg: '#D5F2EC',
+    goodFont: '#2B7163',
+  };
+  
+  export default palette;
