28e51c343350e87ef45a8c3a825be5a63eeb85da
Carl Hipolito <carlarvinhipolito14@gmail.com>
2024-12-08 12:47:41 +0800

    Clone Repository

---
+++ server/app.js
@@ -0,0 +1,52 @@
+import express from 'express'
+
+import { getTable, getRoom } from './be_comlab.js'
+
+const app = express()
+
+app.get("/:table", async (req, res) => {
+    const {table} = req.params
+
+    const get_room = await getTable(table)
+
+    res.send(get_room)
+})
+
+app.get("/laboratories", async (req, res) => {
+    const get_room = await getRoom()
+
+    res.send(get_room)
+})
+
+app.get("/laboratories/:room_id", async (req, res) => {
+    const {room_id} = req.params
+
+    const get_room = await getRoom(room_id)
+
+    res.send(get_room ? get_room : `Room id: ${room_id} doesn't exist`)
+})
+
+app.get("/laboratories/:room_id/room_code", async (req, res) => {
+
+    const {room_id} = req.params
+    const get_room = await getRoom(room_id)
+
+    // Check if room exists
+    if (!get_room) {
+        return res.status(404).send(`Room ID: ${room_id} doesn't exist.`);
+    }
+
+    const {room_code} = get_room
+    res.send(`Room ID: ${room_id} \n Room Code: ${room_code}`)
+    
+})
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+app.listen(8080, () =>{
+    console.log('Server is running on port 8080')
+})
---
+++ server/be_comlab.js
@@ -0,0 +1,106 @@
+// Import mysql2
+import mysql from 'mysql2'
+import dotenv from'dotenv'
+dotenv.config()
+
+// const mysql = require('mysql2')
+// const express = require('express')
+// const app = express()
+// const PORT = 8080;
+
+// //express part
+
+// app.use(express.json())
+
+// app.listen(
+//   PORT,
+//   () => console.log(`it's alive on http://localhost:${PORT}`)
+// )
+
+// app.get('/tshirt', (req, res) => {
+//   res.status(200).send({
+//     tshirt: '👕',
+//     size: 'large'
+//   })
+// });
+
+// app.post('/tshirt/:id', (req, res) => {
+//   const {id} = req.params;
+//   const {logo} = req.body;
+
+//   if(!logo){
+//     res.status(418).send({ message: 'We need a logo!' })
+//   }
+
+//   res.send({
+//     tshirt: `👕 with your ${logo} and ID of ${id}`
+//   })
+
+// });
+
+
+// Create a connection to the database
+
+//const connection = mysql.createConnection({
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST, // XAMPP runs on localhost
+  user: process.env.MYSQL_USER,      // user for XAMPP
+  password: process.env.MYSQL_PASSWORD,      // password for XAMPP
+  database: process.env.MYSQL_DATABASE // database name
+}).promise()
+
+//display a specific table
+export async function getTable(table){
+
+  const [rows] = await pool.query(`SELECT * FROM ${table}`)
+  return rows
+
+}
+
+//ROOM TABLE RELATED QUERY
+
+//[READ] display the room table/ specific room information
+export async function getRoom(room_id = ''){
+  //should be executed when both parameters are provided or none are provided
+    const [rows] = await pool.query(
+      room_id ? `SELECT room_id, CONCAT(room, building_code) AS room_code, total_pc, total_active_pc, total_inactive_pc, total_major_issue, total_minor_issue, total_reports FROM laboratories WHERE room_id = ?` :
+        `SELECT room_id, CONCAT(room, building_code) AS room_code, total_pc, total_active_pc, total_inactive_pc, total_major_issue, total_minor_issue, total_reports FROM laboratories`, [room_id])
+    return room_id ? rows[0] : rows
+
+}
+
+//[CREATE] create a room
+export async function createRoom(room, building_code){
+  const result = await pool.query(`
+    INSERT INTO laboratories(room, building_code, total_pc, total_active_pc, total_inactive_pc, total_major_issue, total_minor_issue, total_reports)
+    VALUES (?, ?, 40, 40, 0, 0, 0, 0
+    )`, [room, building_code])
+  return getRoom(room, building_code) //check if it successfully inserted the values in the db
+}
+
+
+//CONSOLE.LOG PART (FOR TESTING) > > > > > >
+
+// const room = await getRoom(4)
+// console.log(room)
+
+// function insertSample(){
+// // Insert data into the table
+// const insertQuery = `INSERT INTO non_consumable_components (component_id, reference_id, location, specs, flagged) VALUES (?, ?, ?, ?, ?)`;
+// const values = ['MON-001', null, 'location 1', '23/24-inch screen in 16:9 format: resolution of 1920 x 1080 pixels', 0];
+// values[1] = values[0].startsWith('SYU') ? 1 : values[0].startsWith('MON') ? 2 : null //tentative
+
+// pool.query(insertQuery, values, (err, results) => {
+//   if (err) {
+//     console.error('Error inserting data:', err.message);
+//     return;
+//   }
+//   console.log('Data inserted successfully:', results);
+// });
+// }
+
+// // insertSample()
+// // sampleQuery()
+
+// // // Close the connection
+// // connection.end();
